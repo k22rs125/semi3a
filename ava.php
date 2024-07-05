9a892c2c91c0f0dc1a840f09fab37dfb2578dc8c
HondaFumika <169234726+HondaFumika@users.noreply.github.com>
2024-07-05 20:49:33 +0900

    関数作成

---
+++ ava.php
@@ -0,0 +1,18 @@
+<?php
+
+    function getSchedule($year,$month){
+        $regular = [];
+        if($month == 12){
+            $making = [23,24,25];
+        }
+    }
+
+    function getReserve($year,$month){
+        
+    }
+
+    function Available($year,$month){
+
+    }
+
+?>
