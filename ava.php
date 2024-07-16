5fa7b00dbd74e1ddbe37e726e5b544ae82ccace6
HondaFumika <169234726+HondaFumika@users.noreply.github.com>
2024-07-16 17:51:18 +0900

    多次元配列利用

--- ava.php
+++ ava.php
@@ -1,19 +1,28 @@
 <?php
 
-    function getSchedule($year){
-        if($year == 2024){
-            $regular = [5,10,15,20,25,30];
-        }else if($year == 2025){
-            $regular = [4,8,12,16,20,24,28];
+    function getSchedule($year,$month){
+        $data=[
+           2024=>[
+                1 => [2,5,10,15,20]
+            ]
+        ];
+        if(isset($data[$year][$month])){
+            return $data[$year][$month];
         }
-        return $regular;
+
+        return[];
         
     }
 
     function getReserve($year,$month){
-        if($year == 2024 && $month == 1){
-            $reserve = [2,5,10];
-        }
-
-        return $reserve;
+        $data=[
+            2024=>[
+                 1 => [8,9,21,23,24,30]
+             ]
+         ];
+         if(isset($data[$year][$month])){
+             return $data[$year][$month];
+         }
+ 
+         return[];
     }
